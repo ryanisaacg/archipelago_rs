e20192544b3da41412d42234c5ba07755e5e71a0
Ryan Goldstein <ryan@ryanisaacg.com>
2022-10-26 22:23:52 -0400

    Add an example

---
+++ examples/hello_world.rs
@@ -0,0 +1,34 @@
+use archipelago_rs::client::ArchipelagoClient;
+use std::io::{self, BufRead};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    // Connect to AP server
+    let server = prompt("Connect to what AP server?")?;
+
+    let mut url = String::new();
+    url.push_str("ws://");
+    url.push_str(&server);
+
+    let mut client = ArchipelagoClient::new(&url).await?;
+    println!("Connected!");
+
+    // Connect to a given slot on the server
+
+    let game = prompt("What game?")?;
+    let slot = prompt("What slot?")?;
+    client.connect(&game, &slot, None, Some(7), vec!["AP".to_string()]).await?;
+    println!("Connected to slot!");
+
+    client.say("Hello, world!").await?;
+    println!("Sent Hello, world!");
+
+    Ok(())
+}
+
+fn prompt(text: &str) -> Result<String, anyhow::Error> {
+    println!("{}", text);
+
+    Ok(io::stdin().lock().lines().next().unwrap()?)
+}
+
