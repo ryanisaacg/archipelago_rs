89580f293274cdbe34ef3dbb9c3b431b6e986bd7
Ryan Goldstein <ryan@ryanisaacg.com>
2022-10-26 22:28:01 -0400

    Add a pitiful amount of crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! A Rust library that for the [Archipelago game randomizer](archipelago.gg), that implements the [Archipelago network protocol](https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/network%20protocol.md)
+//! Check out ArchipelagoClient for the meat of the logic
+
 pub mod protocol;
 pub mod client;
 
